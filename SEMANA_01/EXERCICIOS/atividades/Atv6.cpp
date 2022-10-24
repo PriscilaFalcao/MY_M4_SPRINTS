0b0570d216f03b44fdf1295a1bc068a65fe27d9f
Priscila Falcão <priscila.santos@sou.inteli.edu.br>
2022-10-24 06:50:31 -0300

    Atividades semana 1

--- SEMANA_01/EXERCICIOS/atividades/Atv6.cpp
+++ SEMANA_01/EXERCICIOS/atividades/Atv6.cpp
@@ -11,8 +11,13 @@ char movimentacao(){
 
 // Variáveis
     int confirma = 0;
-    int Vetor[4]; 
-    char direcaoTecla;
+    int continua;
+    int Vetor[4] = {0, 0 , 0 , 0}; 
+    string direcaoTecla;
+    string direitaTecla = "d";
+    string esquerdaTecla = "a";
+    string frenteTecla = "w";
+    string trasTecla = "s";
 
 // Direção
     struct direcao
@@ -28,12 +33,60 @@ char movimentacao(){
     {
         cout << "Qual direção deseja ir?" << endl;
         cin >> direcaoTecla;
-        if (direcaoTecla == "w")
+        if (strcasecmp(direitaTecla.c_str(), direcaoTecla.c_str()) == 0)
         {
-            /* code */
+            direita.posicao = direita.posicao + 1;
+            cout << "Mapeamento feito: " << direita.direcaoNome << ": " << direita.posicao << ", " << esquerda.direcaoNome << ": " << esquerda.posicao << ", " << frente.direcaoNome << ": " << frente.posicao << ", " << tras.direcaoNome << ": " << tras.posicao << endl;
+            cout << "Última posicao: " << direita.direcaoNome << endl;
+        }
+        else if (strcasecmp(esquerdaTecla.c_str(), direcaoTecla.c_str()) == 0)
+        {
+            esquerda.posicao = esquerda.posicao + 1;
+            cout << "Mapeamento feito: " << direita.direcaoNome << ": " << direita.posicao << ", " << esquerda.direcaoNome << ": " << esquerda.posicao << ", " << frente.direcaoNome << ": " << frente.posicao << ", " << tras.direcaoNome << ": " << tras.posicao << endl;
+            cout << "Última posicao: " << esquerda.direcaoNome << endl;
+        }
+        else if (strcasecmp(frenteTecla.c_str(), direcaoTecla.c_str()) == 0)
+        {
+            frente.posicao = frente.posicao + 1;
+            cout << "Mapeamento feito: " << direita.direcaoNome << ": " << direita.posicao << ", " << esquerda.direcaoNome << ": " << esquerda.posicao << ", " << frente.direcaoNome << ": " << frente.posicao << ", " << tras.direcaoNome << ": " << tras.posicao << endl;
+            cout << "Última posicao: " << frente.direcaoNome << endl;
+        }
+        else if (strcasecmp(trasTecla.c_str(), direcaoTecla.c_str()) == 0)
+        {
+            tras.posicao = tras.posicao + 1;
+            cout << "Mapeamento feito: " << direita.direcaoNome << ": " << direita.posicao << ", " << esquerda.direcaoNome << ": " << esquerda.posicao << ", " << frente.direcaoNome << ": " << frente.posicao << ", " << tras.direcaoNome << ": " << tras.posicao << endl;
+            cout << "Última posicao: " << tras.direcaoNome << endl;
         }
         
+        cout << "Deseja continuar movimentação? (sim: 0 / não: 1) " << endl;
+        cin >> continua;
 
+        if (continua == 1)
+        {
+            confirma = 1;
+            if (Vetor[0]>Vetor[1] && Vetor[0]>Vetor[2]&& Vetor[0]>Vetor[3])
+            {
+                cout << "Maior direcao: "<< direita.direcaoNome << ": " << direita.posicao << endl;
+            }
+            else if (Vetor[1]>Vetor[0] && Vetor[1]>Vetor[2]&& Vetor[1]>Vetor[3])
+            {
+                cout << "Maior direcao: "<< esquerda.direcaoNome << ": " << esquerda.posicao << endl;
+            }
+            else if (Vetor[2]>Vetor[0] && Vetor[2]>Vetor[1] && Vetor[2]>Vetor[3])
+            {
+                cout << "Maior direcao: "<< frente.direcaoNome << ": " << frente.posicao << endl;
+            }
+            else if (Vetor[3]>Vetor[0] && Vetor[3]>Vetor[1]&& Vetor[3]>Vetor[2])
+            {
+                cout << "Maior direcao: "<< tras.direcaoNome << ": " << tras.posicao << endl;
+            }
+        }
     }
-    
+    return 0;
+}
+
+int main()
+{
+    movimentacao();
+    return 0;
 }
